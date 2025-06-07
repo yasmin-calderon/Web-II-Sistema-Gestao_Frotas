81e118504ef31747c2cbaeb7edd268a9d77068c3
tiguassa <tiagofrancischini@gmail.com>
2025-06-07 12:08:39 -0300

    salvando trabalho local antes de atualizar

---
+++ src/app/motorista/motorista-dashboard/motorista-dashboard.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-motorista-dashboard',
+  imports: [],
+  templateUrl: './motorista-dashboard.component.html',
+  styleUrl: './motorista-dashboard.component.css'
+})
+export class MotoristaDashboardComponent {
+
+}
